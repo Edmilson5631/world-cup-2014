5e5a59162e99ea6d02df17ed92de29c1f8783e72
Edmilson Santos <121901468+Edmilson5631@users.noreply.github.com>
2024-09-07 18:26:43 -0300

    styles
    new update

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log("Olà mundo!");
+function pesquisar() {
+  // Obtém a seção HTML onde os resultados serão exibidos
+  const section = document.getElementById("resultados-pesquisa");
+
+  // Obtém o valor do campo de pesquisa, converte para minúsculas para tornar a busca case-insensitive
+  const campoPesquisa = document
+    .getElementById("campo-pesquisa")
+    .value.toLowerCase();
+
+  // Se o campoPesquisa for uma string sem nada
+  if (campoPesquisa == "") {
+    section.innerHTML = "Nenhum resultado encontrado.";
+    return;
+  }
+
+  // Inicializa uma string vazia para armazenar os resultados da pesquisa
+  let resultados = "";
+
+  // Itera sobre cada dado na lista de dados
+  for (const dado of dados) {
+    // Verifica se o título do dado contém o termo de pesquisa (ignorando maiúsculas e minúsculas)
+    if (
+      dado.titulo.toLowerCase().includes(campoPesquisa) ||
+      dado.descricao.includes(campoPesquisa)
+    ) {
+      // Cria um novo elemento HTML para o resultado, destacando o termo pesquisado em negrito
+      resultados += `
+          <div class="item-resultado">
+            <h2><a href="#">${dado.titulo.replace(
+              new RegExp(campoPesquisa, "gi"),
+              (match) => `<strong>${match}</strong>`
+            )}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+          </div>
+        `;
+    }
+  }
+
+  // Atribui os resultados à seção HTML ou exibe uma mensagem caso não haja resultados
+  section.innerHTML = resultados || "<p>Nenhum resultado encontrado.</p>";
+}
---
+++ dados.js
@@ -0,0 +1,50 @@
+let dados = [
+  {
+    titulo: "Neymar",
+    descricao:
+      "Neymar Jr. é um jogador de futebol brasileiro, nascido em 1992,  conhecido por sua habilidade, velocidade e dribles.Atua como atacante, geralmente na ala esquerda, e tem 1,75 metros de altura com um estilo visual marcado por cortes de cabelo e tatuagens.",
+    link: "https://pt.wikipedia.org/wiki/Neymar",
+  },
+
+  {
+    titulo: "Cristiano Ronaldo",
+    descricao:
+      "Cristiano Ronaldo é um dos maiores jogadores de futebol de todos os tempos, conhecido por sua excepcional habilidade, velocidade, força física e precisão nos chutes.",
+    link: "https://pt.wikipedia.org/wiki/Cristiano_Ronaldo",
+  },
+
+  {
+    titulo: "Robben",
+    descricao:
+      " Arjen Robben é um ex-jogador de futebol holandês, amplamente reconhecido por sua velocidade, drible e habilidade em cortar para dentro e finalizar com a perna esquerda.",
+    link: "https://pt.wikipedia.org/wiki/Arjen_Robben",
+  },
+
+  {
+    titulo: "Van persie",
+    descricao:
+      " Robin van Persie é um ex-jogador de futebol holandês, conhecido por sua habilidade como atacante e precisão nas finalizações.",
+    link: "https://pt.wikipedia.org/wiki/Robin_van_Persie",
+  },
+
+  {
+    titulo: "Thiago silva",
+    descricao:
+      "Thiago Silva é um renomado zagueiro brasileiro, conhecido por sua  carreira de sucesso tanto em clubes europeus quanto na seleção brasileira",
+    link: "https://pt.wikipedia.org/wiki/Thiago_Silva_(futebolista)",
+  },
+
+  {
+    titulo: "Marcelo",
+    descricao:
+      " Marcelo Vieira é um icônico lateral-esquerdo brasileiro, amplamente reconhecido por sua habilidade técnica, criatividade ofensiva e visão de jogo.",
+    link: "https://pt.wikipedia.org/wiki/Marcelo_Vieira_da_Silva_J%C3%BAnior",
+  },
+
+  {
+    titulo: "Messi",
+    descricao:
+      " Lionel Messi é um dos maiores jogadores de futebol da história, conhecido por sua habilidade técnica extraordinária, visão de jogo e precisão em dribles e finalizações.",
+    link: "https://pt.wikipedia.org/wiki/Lionel_Messi",
+  },
+];
